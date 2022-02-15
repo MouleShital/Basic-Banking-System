637017aa1d37091b8b32683daad39583f39ac78f
MouleShital <99590393+MouleShital@users.noreply.github.com>
2022-02-15 17:38:34 +0530

    Add files via upload

---
+++ spark/navbar.php
@@ -0,0 +1,22 @@
+<!-- navbar  -->
+<nav class="navbar navbar-expand-md navbar-light bg-light">
+      <a class="navbar-brand" href="index.php" style="color : #000000;"><b>Sparks<span>BANK</span></b></a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#collapsibleNavbar">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+        <div class="collapse navbar-collapse" id="collapsibleNavbar">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item">
+                <a class="nav-link" href="index.php"><b>Home</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="createuser.php"><b>Create User</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transfermoney.php"><b>Transfer Money</b></a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="transactionhistory.php"><b>Transaction History</b></a>
+              </li>
+        </div>
+ </nav>
